7d364d34d422539549bf748dae20ca496171efd2
bsapin <bsapin00@fesb.hr>
2016-03-01 21:32:34 +0100

    Create project

---
+++ GetMeInTheMood/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetMeInTheMood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Moods()
+        {
+
+            return View();
+        }
+
+        public ActionResult MostPopular()
+        {
+            return View();
+        }
+    }
+}
