db1a13751f363d9afdc9205ad41c9b6bd1085ff0
bsapin <bsapin00@fesb.hr>
2016-03-02 12:34:24 +0100

    Added Model Class

---
+++ GetMeInTheMood/Models/Mood.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetMeInTheMood.Models
+{
+    public class Mood
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
