6f3b182ad40b3eaec5130ac2cb6410d120fc3934
boksimamba <boriskostres92@gmail.com>
2016-03-08 13:54:48 +0100

    Login form validation

--- GetMeInTheMood/Models/User.cs
+++ GetMeInTheMood/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +10,17 @@ namespace GetMeInTheMood.Models
     {
         public int ID { get; set; }
 
+        [Required]
+        [EmailAddress]
+        [StringLength(160, MinimumLength = 2)]
         public string email { get; set; }
 
+        [Required]
+        [StringLength(160, MinimumLength = 2)]
         public string username { get; set; }
 
+        [Required]
+        [StringLength(160, MinimumLength = 8)]
         public string password { get; set; }
 
         public List<Playlist> Playlists { get; set; }
