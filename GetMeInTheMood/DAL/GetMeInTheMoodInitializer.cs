4b2abf6249652fdb4dda671cfdb062fc69f8d139
boksimamba <boriskostres92@gmail.com>
2016-03-08 13:02:48 +0100

    Added sha256 values for users

--- GetMeInTheMood/DAL/GetMeInTheMoodInitializer.cs
+++ GetMeInTheMood/DAL/GetMeInTheMoodInitializer.cs
@@ -13,8 +13,18 @@ namespace GetMeInTheMood.DAL
         {
             var users = new List<User>()
             {
-                new User() {email = "berislav.sapina@gmail.com", username="bero", password="bero1993" },
-                new User() {email = "boriskostres92@gmail.com", username="boki", password="boki1991" }
+                new User() {
+                    /* email = "berislav.sapina@gmail.com", username="bero", password="bero1993"*/
+                    email = "1913ec27810b186f6ce6b83a8a2d211e4d167b08c4c4030b0db09a3d24e492cf",
+                    username = "bero",
+                    password = "7418a4b3814752a06da82d8bf329a5ee7ef92073efd22b112b4d7a03b51726d0"
+                           },
+                new User() {
+                    /*email = "boriskostres92@gmail.com", username="boki", password="boki1991"*/
+                    email = "0268358dc84e14217be3a5f406ff3f4cb4a0611bd5aa18ff6a932d2033fbe730",
+                    username = "boki",
+                    password = "da6e2fe0d5c605a95e5f2f2d162af47e642960b1ad71fa94e1d8d95311503471"
+                           }
             };
 
             users.ForEach(user => context.Users.Add(user));
